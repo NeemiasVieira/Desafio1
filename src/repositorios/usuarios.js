81b3ee53a9cc25566ff4a506a406dabe687d0952
Neemias Vieira <neemias.vieira@live.com>
2023-08-08 16:04:18 -0300

    Lista de usuários para testes adicionada

--- src/repositorios/usuarios.js
+++ src/repositorios/usuarios.js
@@ -1,4 +1,47 @@
 //Lista de usuários cadastrados na memória (para usar como exemplo)
 
 //O array de usuários foi removido devido a refatoração implementando o banco de dados
-const usuarios = []
+const usuarios = [
+	{
+		id: "64d29116a64a8129d501abdc",
+		email: "danilo@modalgr.com.br",
+		senha: "Batata123@",
+		dataDeCriacao: "08/08/2023 | 19:01:41"
+	},
+	{
+		id: "64d29101a64a8129d501abd6",
+		email: "diogo@modalgr.com.br",
+		senha: "Batata123@",
+		dataDeCriacao: "08/08/2023 | 19:01:21"
+	},
+	{
+		id: "64d29128a64a8129d501abde",
+		email: "ellen@modalgr.com.br",
+		senha: "Batata123@",
+		dataDeCriacao: "08/08/2023 | 19:02:00"
+	},
+	{
+		id: "64d2910ea64a8129d501abda",
+		email: "isaac@modalgr.com.br",
+		senha: "Batata123@",
+		dataDeCriacao: "08/08/2023 | 19:01:34"
+	},
+	{
+		id: "64d2912ea64a8129d501abe0",
+		email: "murilo@modalgr.com.br",
+		senha: "Batata123@",
+		"dataDeCriacao": "08/08/2023 | 19:02:06"
+	},
+	{
+		id: "64d29108a64a8129d501abd8",
+		email: "neemias@modalgr.com.br",
+		senha: "Batata123@",
+		dataDeCriacao: "08/08/2023 | 19:01:28"
+	},
+	{
+		id: "64d290f9a64a8129d501abd4",
+		email: "pedro@modalgr.com.br",
+		senha: "Batata123@",
+		dataDeCriacao: "08/08/2023 | 19:01:13"
+	}
+]
